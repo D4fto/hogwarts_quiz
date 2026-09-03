209d578a15df53bf030469b5e603161dec5b49d4
Pedro Tunes <163424876+D4fto@users.noreply.github.com>
2026-09-02 22:48:42 -0300

    feat: Primeira aula - Home Screen

--- app/src/main/java/com/example/template/screen/HomeScreen.kt
+++ app/src/main/java/com/example/template/screen/HomeScreen.kt
@@ -1,12 +1,20 @@
 package com.example.template.screen
 
+import android.graphics.drawable.PaintDrawable
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -14,12 +22,28 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.template.R
 import com.example.template.ui.theme.AndroidTemplateTheme
+import com.example.template.ui.theme.GryffindorRed
+import com.example.template.ui.theme.HufflepuffYellow
+import com.example.template.ui.theme.RavenclawBlue
+import com.example.template.ui.theme.SlytherinGreen
 
+
+enum class Casa(val casa: String){
+    GRIFINORIA(casa = "Grifinória"),
+    SONSERIA(casa = "Sonserina"),
+    LUFALUFA(casa = "Lufa Lufa"),
+    CORNVINAL(casa = "Cornvinal");
+
+    override fun toString(): String = casa
+    
+}
 /**
  * Única tela do template. Comece a construir seu app a partir daqui.
  */
@@ -31,26 +55,64 @@ fun HomeScreen(
         modifier = modifier.fillMaxSize()
     ) { innerPadding ->
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .padding(24.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
+           modifier = Modifier
+               .fillMaxSize()
+               .padding(paddingValues = innerPadding)
+               .padding(all = 24.dp),
             horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+        ){
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ){
+
+                Text(
+                    text = "Hogwarts Quiz"
+                )
+                Text(
+                    text = "0/5"
+                )
+            }
+            Spacer(modifier = Modifier.height(60.dp))
             Image(
                 painter = painterResource(R.drawable.image),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(180.dp)
-                    .clip(RoundedCornerShape(16.dp))
+                contentDescription = "Imagem do Harry Potter"
+
             )
-            Text("Tudo pronto 🎉", style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.height(80.dp))
             Text(
-                "Edite screen/HomeScreen.kt para começar.",
-                style = MaterialTheme.typography.bodyMedium
+                text = "Harry Potter",
+                fontSize = 32.sp
             )
+            Text(
+                text = "Escolha sua casa"
+            )
+            Spacer(modifier = Modifier.height(60.dp))
+            for (value in Casa.entries){
+                CasaButton(value)
+            }
+
         }
+    }
+}
+
+@Composable
+fun CasaButton(casa: Casa){
+    Button(
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            contentColor = Color.White,
+            containerColor = when(casa) {
+                Casa.GRIFINORIA -> GryffindorRed
+                Casa.SONSERIA -> SlytherinGreen
+                Casa.LUFALUFA -> HufflepuffYellow
+                Casa.CORNVINAL -> RavenclawBlue
+            }
+        ),
+        onClick = {}
+    ) {
+        Text(casa.toString())
     }
 }
 
---
+++ app/src/main/java/com/example/template/screen/Resultado.kt
@@ -0,0 +1,33 @@
+package com.example.template.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.template.ui.theme.AndroidTemplateTheme
+
+@Composable
+fun Resultado(
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        modifier = modifier.fillMaxSize()
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues = innerPadding)
+        ) { }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    AndroidTemplateTheme {
+        HomeScreen()
+    }
+}
